de8602d389edb2b029db1e635596e1fb27b0f1d8
kapilan1 <kapilan@codelantic.com>
2023-03-23 18:10:21 +0530

    function modified

--- src/test/java/MyUtilsTest.java
+++ src/test/java/MyUtilsTest.java
@@ -7,8 +7,11 @@ public class MyUtilsTest {
     @Test
     void addMockitoSimpleBehaviourTest1(){
         MyUtils mock1 =  mock(MyUtils.class);
-        when(mock1.add(45,25)).thenReturn(70);
-        assertEquals(70,mock1.add(45,25));
+        when(mock1.add(40,30)).thenReturn(70);
+        doReturn(60).when(mock1).add(28,32);
+
+        assertEquals(70,mock1.add(40,30));
+        assertEquals(60,mock1.add(28,32));
     }
 
     @Test
@@ -20,7 +23,24 @@ public class MyUtilsTest {
 
     @Test
     void addMockitoconsecutiveMethodCallsTest(){
-        
+        MyUtils mock1 =  mock(MyUtils.class);
+        when(mock1.add(anyInt(),anyInt())).thenReturn(35,40,45);
+
+        assertEquals(35,mock1.add(25,10));
+        assertEquals(40,mock1.add(20,20));
+        assertEquals(45,mock1.add(9,36));
 
     }
+
+    @Test
+    void multiplyMockitoconsecutiveMethodCallsTest(){
+        MyUtils mock1 =  mock(MyUtils.class);
+        when(mock1.multiply(anyInt(),anyInt())).thenReturn(36,80,42);
+
+        assertEquals(36,mock1.multiply(6,6));
+        assertEquals(80,mock1.multiply(3,8));
+        assertEquals(42,mock1.multiply(21,2));
+
+    }
+
 }
